15a2ab4e9649f24865e7b24757b33e0b90b763af
Kada Omar <kadaomar@hotmail.com>
2020-10-11 17:23:19 +0200

    Initial commit

---
+++ precache-manifest.c87ebab6dd29248e2c1feae459f015da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e39c8049ae9c245efaf610025666566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a12ba0a0f3e116a5977b",
+    "url": "/static/css/main.9d014527.chunk.css"
+  },
+  {
+    "revision": "02344cebdbb864b0152a",
+    "url": "/static/js/2.feb0a70a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.feb0a70a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12ba0a0f3e116a5977b",
+    "url": "/static/js/main.a502a66e.chunk.js"
+  },
+  {
+    "revision": "71e5e43089d5c4a75a01",
+    "url": "/static/js/runtime-main.1e09b5e4.js"
+  }
+]);
