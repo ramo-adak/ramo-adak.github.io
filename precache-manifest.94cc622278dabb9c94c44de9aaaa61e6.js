0d9bedf031f903a680b995625db4be5b1fe869e1
Zakaria@HP <zmellal92@gmail.com>
2020-10-29 00:06:30 +0100

    deploy new version

---
+++ precache-manifest.94cc622278dabb9c94c44de9aaaa61e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd1076c6475f9f62ab52cdabefa0ac5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f02e4b47017cbcbcb49",
+    "url": "/static/css/main.ef5256d3.chunk.css"
+  },
+  {
+    "revision": "9ecd0bb908aca6daf657",
+    "url": "/static/js/2.46f2cfa5.chunk.js"
+  },
+  {
+    "revision": "6fea63d60fdb8b8fedaa8ddcb8d5d42e",
+    "url": "/static/js/2.46f2cfa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f02e4b47017cbcbcb49",
+    "url": "/static/js/main.71e55efd.chunk.js"
+  },
+  {
+    "revision": "71e5e43089d5c4a75a01",
+    "url": "/static/js/runtime-main.1e09b5e4.js"
+  }
+]);
